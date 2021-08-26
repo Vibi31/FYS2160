07a7553e361d27810a79eb412693d3eb2c8b9c5b
vibishar <vibishar@mail.uio.no>
2021-08-26 13:07:59 +0200

    update

--- LAMMPS_analysis.py
+++ LAMMPS_analysis.py
@@ -1,2 +1,4 @@
-import lammps as lmp 
- 
+
+import lammps_logfile
+log = lammps_logfile.File(\"./log.lammps\")
+t = log.get(\"Time\")
--- LAMMPS_log_plot.py
+++ LAMMPS_log_plot.py
@@ -1,4 +1,11 @@
 import matplotlib.pyplot as plt
 from pylab import *
 
-data = dump("dump.lammpstrj") # Read all timesteps
+data = dump("dump.lammpstrj")       #timestep data
+
+t = data.time()                     #time steps
+nt = size(t)
+print('number of timesteps:', nt)   #just wanted to see
+
+n_particles = zeros(n,float)        #number of atoms
+
