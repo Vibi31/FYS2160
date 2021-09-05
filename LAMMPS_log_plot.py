32c153ca4798695686f9c790a80e1a250da1508f
vibishar <vibishar@mail.uio.no>
2021-09-05 10:37:41 +0200

    updated coed and fixing small bugs + labelled axes

--- LAMMPS_log_plot.py
+++ LAMMPS_log_plot.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
-from pylab import *
+import dump from 
+from numpy import *
+"""
+majority of the code is from statistical physics python-book
 
-data = dump("dump.lammpstrj")       #timestep data
+"""
+data = dmp('dump.lammpstrj')       #timestep data
 
 t = data.time()                     #time steps
 nt = size(t)
@@ -9,3 +13,15 @@ print('number of timesteps:', nt)   #just wanted to see
 
 n_particles = zeros(n,float)        #number of atoms
 
+tmp_time,box,atoms,bonds,tris,lines = data.viz(0)
+halfsize = 0.5*box[3] # Box size in x-di
+
+
+for i in range(nt):
+    xit = array(data.vecs(i,"x"))
+    nleft[i] = size(find(xit<halfsize))
+
+plt.plot(t,n_particles)
+plt.xlabel('t')
+plt.ylabel('N_p')
+plt.show() 
