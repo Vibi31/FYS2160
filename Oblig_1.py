2144020b6ced1df4c9231ae79c7d6b39e4c3a815
Vibi31 <vrvichu0@gmail.com>
2021-09-19 20:53:46 +0200

    Started with oblig 1, data extracted.

--- Oblig_1.py
+++ Oblig_1.py
@@ -1,4 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
 file = open('termokopper.txt', 'r')
 Lines = file.readlines()
+"""
 
-testing
+time = []
+temp1 = []
+temp2 = []
+
+with open('termokopper.txt', 'r') as lines:
+    for line in lines:
+        row = line.split()              #split the line when it finds space
+        time.append(row[0])
+        temp1.append(row[1])
+        temp2.append(row[2])
+
+print(time)
