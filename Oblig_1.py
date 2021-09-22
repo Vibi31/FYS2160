d0912640e11134d8f5a6e8d3109ac3e77331d5b6
Vibi31 <vrvichu0@gmail.com>
2021-09-22 08:47:17 +0200

    Update

--- Oblig_1.py
+++ Oblig_1.py
@@ -7,8 +7,10 @@ Lines = file.readlines()
 """
 
 time = []
-temp1 = []
-temp2 = []
+temp1 = []      #Tw temperature of water
+temp2 = []      #Tw temperature of water
+
+Ta = 22         # ambient temperature
 
 with open('termokopper.txt', 'r') as lines:
     for line in lines:
@@ -16,5 +18,20 @@ with open('termokopper.txt', 'r') as lines:
         time.append(row[0])
         temp1.append(row[1])
         temp2.append(row[2])
+infile.close()
 
-print(time)
+#print(len(time), len(temp1), len(temp2))
+#t = np.linspace(0,len(time), len(time))
+t = np.array(time)
+t1 = np.array(temp1)
+t2 = np.array(temp2)
+
+"""
+plt.plot(np.array(time), np.array(temp1), label="temp1")
+plt.legend()
+plt.show()
+
+plt.plot(time, np.array(temp2), label="temp1")
+plt.legend()
+plt.show()
+"""
