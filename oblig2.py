edb0a251ea80c9ad82ad29c56308f7bbc9044fe6
Vibi31 <vrvichu0@gmail.com>
2021-10-06 09:14:26 +0200

    Oblig 2:  plot for question R, pssibly incorrect

--- oblig2.py
+++ oblig2.py
@@ -4,13 +4,25 @@ from numpy import exp
 
 
 T = np.linspace(1, 1000, 1000) #10,000 data points
-delta_e = 1.602176565e-19 #[1eV]
+delta_e = 1.602176565e-19 #[1eV] in joules
 Kb = 1.38064852e-23 #boltzmann constant
 N = 100 
 n = N/(exp(delta_e/(T*Kb))+1) #vacancies 
 
+"""
+#oppgave Q
 plt.title('vacancies as a function of temperature [K]')
 plt.ylabel('Temperature [K]')
 plt.xlabel('Vacancies [n]')
 plt.plot(n,T)
+plt.show()
+"""
+
+#oppgave R
+Cv = N*delta_e*exp(delta_e/(T*Kb))/(((Kb*T**2)*(exp(delta_e/(T*Kb))+1)**2))#heat capacity [J/K]
+
+plt.title('Heat capacity as a function of temperature')
+plt.xlabel('Heat capacity [J/K]')
+plt.ylabel('Temperature [K]')
+plt.plot(Cv, T)
 plt.show()
